c074a0794d5aedae53d088ca1415bc9a4f5bb73b
ilyasouerdi <ilyasouerdi2@gmail.com>
2023-07-14 07:39:44 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - entry of code
+ *
+ * Return: 0 (secciss)
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0, i < 26, i++)
+	{
+		putchar(alph[i]);
+	}
+}
