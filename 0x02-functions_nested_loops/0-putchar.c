dcf62adc53a1fd22a604555ee98b3ebb576bfdc6
ilyasouerdi <ilyasouerdi2@gmail.com>
2023-07-18 08:54:47 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,14 @@
  */
 int main(void)
 {
-	printf("_putchar");
-	putchar("\n");
-	return (0);
+_putchar('_');
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
