b5a56e63f35fef08cd36a8c95dff71ea4c2dee68
ilyasouerdi <ilyasouerdi2@gmail.com>
2023-07-18 16:51:56 -0700

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
+ */
+void main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 98; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%ld", sum);
+		fib1 = fib2;
+		fib2 = sum;
+		if (count == 98)
+			printf("\n");
+		else if (count < 98)
+			printf(", ");
+	}
+}
